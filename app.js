080eb9e21d59d862891372b67c3da30e97767879
Sebastián Saldaña <A01570274@itesm.mx>
2021-04-25 21:04:36 -0500

    Basic project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express'),
+    app = express(),
+    PORT = 5000 || process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'Hello from index route!' });
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
