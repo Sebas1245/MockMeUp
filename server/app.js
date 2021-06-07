c9054d09deef030398061ae1dbccebdd139de99a
Sebastián Saldaña <sebastian.saldana@wuhutech.com>
2021-06-06 19:05:27 -0500

    Sets up server  to serve static build

--- server/app.js
+++ server/app.js
@@ -3,8 +3,9 @@ const express = require('express'),
     app = express(),
     dbConfig = require('./config/dbSetup'),
     sendAsJSON = require('./middleware/sendAsJson'),
-    eHandler = require('./middleware/errorHandler'),
-    PORT = 5000 || process.env.PORT;
+    eHandler = require('./middleware/errorHandler');
+
+const PORT = 5000 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,6 +13,9 @@ app.use(require('cors')());
 
 // initialize db 
 dbConfig();
+
+// Serves build
+app.use(express.static(path.resolve('./client/build')));
 
 // Auth routes
 app.use('/api', require('./Auth'))
@@ -23,5 +27,13 @@ app.get('/', (req, res) => {
 })
 app.use(eHandler());
 app.use(sendAsJSON());
+// Redirects everything else to index
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./client/build/index.html'));
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve('./client/build/index.html'));
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
